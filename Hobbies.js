2fdb8360a87e9e9bfba8b88423e9977bfdd20205
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2024-02-05 21:33:22 -0800

    Add files via upload

---
+++ Hobbies.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hobbies = () => {
+  return (
+    <div className="hobbies">
+      <h2>Hobbies</h2>
+      <ul>
+        <li>Working out</li>
+        <li>Rowing</li>
+        <li>Hiking</li>
+        <li>Traveling</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Hobbies;
---
+++ Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <img src="profile-image.jpeg" alt="Profile" />
+      <p>Hello, my name is: </p>
+      <h1>Diego Cano</h1>
+    </div>
+  );
+}
+
+export default Profile;
